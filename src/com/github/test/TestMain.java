b3d439047bf7c79e3b5dc194a59c2e8841fac238
Bluegent <bluegentleman@live.com>
2015-05-06 23:12:32 +0300

    Added very basic functionality
    Program reads an integer number n and prints n random numbers between 0
    and 1.

--- src/com/github/test/TestMain.java
+++ src/com/github/test/TestMain.java
@@ -1,7 +1,21 @@
 package com.github.test;
 
+import java.util.Scanner;
+
 public class TestMain {
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
+		Scanner sc= new Scanner(System.in);
+		int n;
+		System.out.print("Input times:");
+		try {
+			n=Utils.tryParseInt(sc.next());
+			for(int i=0;i<n;i++){
+				System.out.println(Math.random());
+			}
+		} catch (Exception e) {
+			System.out.println("Not a number.");
+		}
+		sc.close();
 	}
 }
---
+++ src/com/github/test/Utils.java
@@ -0,0 +1,7 @@
+package com.github.test;
+
+public class Utils {
+	public static int tryParseInt(String str) throws Exception{
+		return Integer.parseInt(str);
+	}
+}
